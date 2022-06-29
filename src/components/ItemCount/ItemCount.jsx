32f7e81699973ac99b6cb584d33153fb392b806c
DanielTorres-github <torres.daniel.adr2@gmail.com>
2022-06-29 14:19:32 -0300

    Reentrega CoderHouse

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,6 +1,6 @@
-import React, {useContext, useState} from 'react';
-import {Link} from 'react-router-dom';
-import {cartContext} from '../../context/cartContext';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { cartContext } from '../../context/cartContext';
 
 function FinalizarCompra() {
     return (
@@ -16,11 +16,11 @@ function FinalizarCompra() {
 }
 
 
-function ItemCount({stock, prod}) {
+function ItemCount({ stock, prod }) {
 
     const [inputState, setInputState] = useState(true)
     const [cont, SetCont] = useState(1)
-    const {addToCart} = useContext(cartContext)
+    const { addToCart } = useContext(cartContext)
 
     function changeStateInput() {
         setInputState(false)
@@ -37,41 +37,26 @@ function ItemCount({stock, prod}) {
             cant: cantidad
         })
     }
-    function Contador({changeStateInput}) {
+    function Contador({ changeStateInput }) {
         return (
             <>
                 <div className='flex justify-center items-center'>
                     <div className="flex flex-auto justify-around m-3 border-2 border-blue-400">
-                        <button onClick={
-                                () => sumarProducto(-1)
-                            }
-                            className="text-blue-500"
-                            disabled={
-                                cont < 2
-                        }>
+                        <button onClick={() => sumarProducto(-1)} className="text-blue-500" disabled={cont < 2}>
                             -
                         </button>
                         <p className="text-center">
-                            {
-                            stock == 0 ? "No hay stock" : cont
-                        }</p>
-                        <button onClick={
-                                () => sumarProducto(1)
-                            }
-                            className="text-blue-500"
-                            disabled={
-                                stock == 0 || cont == stock
-                        }>
+                            {stock === 0 ? "No hay stock" : cont}
+                        </p>
+                        <button onClick={() => sumarProducto(1)} className="text-blue-500" disabled={stock === 0 || cont === stock}>
                             +
                         </button>
                     </div>
                     <p className='text-amber-700 font-medium'>Stock: {stock}</p>
                 </div>
-                <button onClick={changeStateInput}
-                    className="bg-blue-500 rounded m-3 mb-4 pt-1 pb-1 pl-2 pr-2"
-                    disabled={
-                        stock == 0
-                }>Agregar Producto</button>
+                <button onClick={changeStateInput} className="bg-blue-500 rounded m-3 mb-4 pt-1 pb-1 pl-2 pr-2" disabled={stock === 0}>
+                    Agregar Producto
+                </button>
             </>
         )
     }
